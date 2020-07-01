6d83d0c7b2670cd7f91e518c90e34d86e488c89d
Pradeep <pradeep@incaendo.com>
2020-07-01 23:51:52 +0530

    fix: create html for lightbox in js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector('.modal');
+const preview = document.querySelectorAll('.gallary-container img');
+const original = document.querySelector('.full-image');
+const imgText = document.querySelector('.caption-text');
+
+preview.forEach(preview => {
+   preview.addEventListener('click', () =>{
+       modal.classList.add('open');
+       original.classList.add("open");
+       const originalSrc = preview.getAttribute("data-original");
+       original.src = `./images/${originalSrc}`;
+   })
+})
+
+modal.addEventListener('click', (e) => {
+   if(e.target.classList.contains('modal')){
+       modal.classList.remove('open');
+       original.classList.remove("open");
+   }
+})
